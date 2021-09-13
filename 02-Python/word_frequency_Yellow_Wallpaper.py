d75468ebf17fc2b5258935c75f097ecc2eacbd89
melaniewalsh <melanie.walsh@cornell.edu>
2021-09-13 12:05:01 -0700

    first commit

---
+++ 02-Python/word_frequency_Yellow_Wallpaper.py
@@ -0,0 +1,44 @@
+#Import Libraries and Modules
+
+import re
+from collections import Counter
+
+# Define Functions
+
+def split_into_words(any_chunk_of_text):
+    lowercase_text = any_chunk_of_text.lower()
+    split_words = re.split("\W+", lowercase_text)
+    return split_words
+
+# Define Filepaths and Assign Variables
+
+stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours',
+ 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers',
+ 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves',
+ 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are',
+ 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does',
+ 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until',
+ 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into',
+ 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down',
+ 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here',
+ 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',
+ 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so',
+ 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 've', 'll', 'amp']
+
+filepath_of_text = "../texts/literature/The-Yellow-Wallpaper_Charlotte-Perkins-Gilman.txt"
+number_of_desired_words = 40
+
+# Read in File
+
+full_text = open(filepath_of_text).read()
+
+# Manipulate and Analyze File
+
+all_the_words = split_into_words(full_text)
+meaningful_words = [word for word in all_the_words if word not in stopwords]
+meaningful_words_tally = Counter(meaningful_words)
+most_frequent_meaningful_words = meaningful_words_tally.most_common(number_of_desired_words)
+
+# Output Results
+
+print(most_frequent_meaningful_words)
