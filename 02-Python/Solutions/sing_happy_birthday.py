e45d105bed6051934bcbdd7b78ddfbb8f0db0973
melaniewalsh <melanie.walsh@cornell.edu>
2021-10-15 16:19:30 -0700

    add Class 5 exercises solutions

---
+++ 02-Python/Solutions/sing_happy_birthday.py
@@ -0,0 +1,11 @@
+import sys
+
+def sing_happy_birthday(name='Person'):
+    print(f"""
+    Happy birthday to you
+    Happy birthday dear {name}
+    Happy birthday to you""")
+
+user_defined_name = sys.argv[1]
+
+sing_happy_birthday(name=user_defined_name)
